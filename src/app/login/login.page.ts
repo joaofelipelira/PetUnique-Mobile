fff974c754993b1d5f3ce70cfd4d31d263be89c5
Felipe <felypelyrasouz@gmail.com>
2024-12-01 17:41:50 -0300

    Updates css

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
+  emailInvalid: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+    // Aqui você pode adicionar a lógica para fazer login
+    console.log('Email:', this.email);
+    console.log('Senha:', this.password);
+
+    // Exemplo de validação de email
+    this.validateEmail(this.email);
   }
 
+  validateEmail(email: string) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    this.emailInvalid = !emailPattern.test(email);
+  }
 }
