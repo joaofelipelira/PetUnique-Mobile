9441c42cae0db977dd88df3afaeb8409f9226813
Felipe <felypelyrasouz@gmail.com>
2024-12-02 22:39:57 -0300

    Update + adição da tela de Sucesso

--- src/app/success-page/success-page.page.ts
+++ src/app/success-page/success-page.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-page',
   templateUrl: './success-page.page.html',
   styleUrls: ['./success-page.page.scss'],
 })
-export class SuccessPagePage implements OnInit {
+export class SuccessPagePage {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  goToNewsHub() {
+    this.router.navigate(['/news-hub']); // Substitua '/news-hub' pelo caminho correto da sua página de notícias
   }
-
 }
